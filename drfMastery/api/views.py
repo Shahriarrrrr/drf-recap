fe687ec81e5b7b030ed92860480faf357c7c0575
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-10-06 17:48:23 +0600

    PageNumberPagination

--- drfMastery/api/views.py
+++ drfMastery/api/views.py
@@ -10,11 +10,12 @@ from rest_framework.views import APIView
 from .filters import ProductFilter, InStockFilterBackend
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination
 
 
 
 class ProductListCreateApiView(generics.ListCreateAPIView):
-      queryset = Product.objects.all()
+      queryset = Product.objects.order_by('pk')
       serializer_class = ProductSerializer
       filterset_class = ProductFilter
       filter_backends = [DjangoFilterBackend,
@@ -24,6 +25,12 @@ class ProductListCreateApiView(generics.ListCreateAPIView):
                         ]
       search_fields = ['name', 'description', '=price']
       ordering_fields = ['name', 'price', 'stock']
+      pagination_class = PageNumberPagination
+      pagination_class.page_size = 2
+      pagination_class.page_query_param = 'pagenum'
+      pagination_class.page_size_query_param = 'size'
+      pagination_class.max_page_size = 6
+
 
       def get_permissions(self):
             self.permission_classes = [AllowAny]
