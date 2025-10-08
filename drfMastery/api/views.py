9b162a4f742c7c0ac265dd03c5723a67dab101b9
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-10-08 15:42:50 +0600

    actionsDecorator

--- drfMastery/api/views.py
+++ drfMastery/api/views.py
@@ -7,12 +7,12 @@ from rest_framework.decorators import api_view
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser, AllowAny
 from rest_framework.views import APIView
-from .filters import ProductFilter, InStockFilterBackend
+from .filters import ProductFilter, InStockFilterBackend, OrderFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
 
 class ProductListCreateApiView(generics.ListCreateAPIView):
       queryset = Product.objects.order_by('pk')
@@ -71,8 +71,21 @@ class ProductDetailApiView(generics.RetrieveUpdateDestroyAPIView):
 class OrderViewSet(viewsets.ModelViewSet):
       queryset = Order.objects.prefetch_related('items__product')
       serializer_class = OrderSerializer
-      permission_classes = [AllowAny]
+      permission_classes = [IsAuthenticated] #Applied for all
       pagination_class = None
+      filterset_class = OrderFilter
+      filter_backends = [DjangoFilterBackend]
+
+      @action(
+            detail=False, 
+            methods=['get'], 
+            url_path='user-orders',
+            permission_classes = [IsAuthenticated]
+            )
+      def user_orders(self, request):
+            orders = self.get_queryset().filter(user=request.user)
+            serializer = self.get_serializer(orders, many = True)
+            return Response(serializer.data)
 
 
 
